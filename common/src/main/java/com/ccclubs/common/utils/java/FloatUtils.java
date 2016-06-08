c5add6401221d0d7fa90847f6053cc6364910f37
qsxiaogang <513387369@qq.com>
2016-06-08 13:15:43 +0800

    modify minSdk

--- common/src/main/java/com/ccclubs/common/utils/java/FloatUtils.java
+++ common/src/main/java/com/ccclubs/common/utils/java/FloatUtils.java
@@ -6,26 +6,29 @@ import java.math.BigDecimal;
  * Float相关的工具函数
  */
 public class FloatUtils {
-	/**
-	 * 默认保留2位小数
-	 *
-	 * @param d
-	 * @return
-	 */
-	public static float formatFloat(double d) {
-		return formatFloat(d, 2);
-	}
+  /**
+   * 默认保留2位小数
+   */
+  public static float formatFloat(double d) {
+    return formatFloat(d, 2);
+  }
 
-	/**
-	 * 保留小数，
-	 *
-	 * @param d
-	 * @return
-	 */
-	public static float formatFloat(double d, int num) {
-		BigDecimal bd = new BigDecimal(d);
-		bd = bd.setScale(num, BigDecimal.ROUND_HALF_UP);
-		return bd.floatValue();
-	}
+  /**
+   * 保留两位小数，
+   */
+  public static float formatFloat(double d, int num) {
+    BigDecimal bd = new BigDecimal(d);
+    bd = bd.setScale(num, BigDecimal.ROUND_HALF_UP);
+    return bd.floatValue();
+  }
 
+  public static float formatFloat(String i) {
+    try {
+      BigDecimal b = new BigDecimal(i);
+      BigDecimal bb = b.setScale(2, BigDecimal.ROUND_HALF_UP);
+      return bb.floatValue();
+    } catch (Exception e) {
+      return 0.00f;
+    }
+  }
 }
